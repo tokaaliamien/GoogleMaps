facd8743d4e0884b14d79a7b59bf41758ceac824
tokaaliamien <tokaaliamien@example.com>
2018-07-25 17:47:15 +0200

    Get the closest city

--- app/src/main/java/com/example/android/googlemapstask/Activities/MapsActivity.java
+++ app/src/main/java/com/example/android/googlemapstask/Activities/MapsActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.android.googlemapstask.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -21,6 +22,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -117,6 +119,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
                                     new LatLng(mLastKnownLocation.getLatitude(),
                                             mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            mMap.setMyLocationEnabled(true);
+                            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                            getClosestCity();
                         } else {
                             Log.d(LOG_TAG, "current location is null using default");
                             Log.e(LOG_TAG, "Exception:" + task.getException());
@@ -127,5 +132,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } catch (SecurityException e) {
             Log.e(LOG_TAG, "Exception:" + e.getMessage());
         }
+    }
+
+    private void getClosestCity() {
+        Location cairo, luxor;
+
+        cairo = new Location("cairo");
+        cairo.setLatitude(30.051149);
+        cairo.setLongitude(31.236146);
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(cairo.getLatitude(), cairo.getLongitude()))
+                .title("Cairo"));
+
+        luxor = new Location("luxor");
+        luxor.setLatitude(25.690760);
+        luxor.setLongitude(32.639554);
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(luxor.getLatitude(), luxor.getLongitude()))
+                .title("Luxor"));
+
+        double distanceToCairo = calculateDistance(mLastKnownLocation, cairo);
+        double distanceToLuxor = calculateDistance(mLastKnownLocation, luxor);
+
+        TextView cairoTextView = findViewById(R.id.tv_cairo_distance);
+        TextView luxorTextView = findViewById(R.id.tv_luxor_distance);
+        TextView closerCityTextView = findViewById(R.id.tv_closer_city);
+
+        cairoTextView.setText(Double.toString(distanceToCairo));
+        luxorTextView.setText(Double.toString(distanceToLuxor));
+
+        if (distanceToCairo < distanceToLuxor)
+            closerCityTextView.setText("Cairo");
+        else
+            closerCityTextView.setText("Luxor");
+
+    }
+
+
+    private double calculateDistance(Location myLocation, Location cityLocation) {
+        return myLocation.distanceTo(cityLocation);
     }
 }
